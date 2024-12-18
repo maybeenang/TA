def46587233eab13b714f0ac9189d0f12faa96b5
maybeenang <muhammad.120140194@student.itera.ac.id>
2024-12-18 19:04:19 +0700

    feat: add color dashboard card

--- app/Services/SidebarService.php
+++ app/Services/SidebarService.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Facades\Cache;
 
 class SidebarService
 {
+
+    protected AcademicYearService $academicYearService;
+    public function __construct()
+    {
+        $this->academicYearService = app(AcademicYearService::class);
+    }
+
     public function getBadgeCount()
     {
 
@@ -19,12 +26,23 @@ class SidebarService
         $gkmpLaporanVerifikasi = $this->query()
             ->where('signature_gkmp_id', null)->count();
 
-
+        $buatLaporanCount = Report::query()
+            ->with(['classRoom', 'reportStatus'])
+            ->whereHas('classRoom', function ($query) {
+                $query->where('academic_year_id', $this->academicYearService->getCurrentAcademicYear()->id);
+                $query->whereHas('lecturer.user', fn($query) => $query->where('id', auth()->id()));
+            })
+            ->whereHas('reportStatus', function ($query) {
+                $query->where('name', '!=', ReportStatusEnum::TERVERIFIKASI);
+                $query->where('name', '!=', ReportStatusEnum::DIKIRIM);
+            })
+            ->count();
 
         return [
             'adminLaporanVerifikasi' => $adminLaporanVerifikasi,
             'gkmpLaporanVerifikasi' => $gkmpLaporanVerifikasi,
             'kaprodiLaporanVerifikasi' => $kaprodiLaporanVerifikasi,
+            'buatLaporanCount' => $buatLaporanCount
         ];
     }
 
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,22 +1,26 @@
 <x-app-layout>
     <div class="grid h-fit w-full grid-cols-2 gap-4 md:grid-cols-4">
-        <x-card.dashboard-card :number="$academicYearNow->fullName">Tahun Ajaran</x-card.dashboard-card>
-        @role("admin")
-            <x-card.dashboard-card :number="$dashboardData->laporanCount">Jumlah Laporan</x-card.dashboard-card>
-            <x-card.dashboard-card :number="$dashboardData->laporanBelumSelesaiCount">
+        <x-card.dashboard-card :number="$academicYearNow->fullName" class="bg-blue-500 text-white">
+            Tahun Ajaran
+        </x-card.dashboard-card>
+        @role('admin')
+            <x-card.dashboard-card :number="$dashboardData->laporanCount" class="bg-green-500 text-white">
+                Jumlah Laporan
+            </x-card.dashboard-card>
+            <x-card.dashboard-card :number="$dashboardData->laporanBelumSelesaiCount" class="bg-amber-500 text-white">
                 Laporan Belum Selesai
             </x-card.dashboard-card>
-            <x-card.dashboard-card :number="$dashboardData->laporanSudahSelesaiCount">
+            <x-card.dashboard-card :number="$dashboardData->laporanSudahSelesaiCount" class="bg-red-500 text-white">
                 Laporan Sudah Selesai
             </x-card.dashboard-card>
         @else
-            <x-card.dashboard-card :number="$selfLecturer?->classroomCount">
+            <x-card.dashboard-card :number="$selfLecturer?->classroomCount" class="bg-green-500 text-white">
                 Jumlah kelas yang diampu
             </x-card.dashboard-card>
-            <x-card.dashboard-card :number="$selfLecturer?->laporanBelumSelesaiCount">
+            <x-card.dashboard-card :number="$selfLecturer?->laporanBelumSelesaiCount" class="bg-amber-500 text-white">
                 Jumlah Laporan Belum Selesai
             </x-card.dashboard-card>
-            <x-card.dashboard-card :number="$selfLecturer?->laporanSelesaiCount">
+            <x-card.dashboard-card :number="$selfLecturer?->laporanSelesaiCount" class="bg-red-500 text-white">
                 Jumlah Laporan Sudah Selesai
             </x-card.dashboard-card>
         @endrole
@@ -24,7 +28,7 @@
 
         <x-card.dashboard-informasi-card />
 
-        @role("admin")
+        @role('admin')
             <livewire:grid.admin-verifikasi-laporan />
         @endrole
 
