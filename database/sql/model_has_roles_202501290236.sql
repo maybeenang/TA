ebea8585ee496c586f4a5a3579dd10caf50a9708
maybeenang <muhammad.120140194@student.itera.ac.id>
2025-01-29 02:37:51 +0700

    update

---
+++ database/sql/model_has_roles_202501290236.sql
@@ -0,0 +1,38 @@
+INSERT INTO sispela.model_has_roles (role_id,model_type,model_id) VALUES
+	 (1,'App\Models\User',1),
+	 (2,'App\Models\User',2),
+	 (3,'App\Models\User',3),
+	 (3,'App\Models\User',4),
+	 (3,'App\Models\User',5),
+	 (3,'App\Models\User',6),
+	 (3,'App\Models\User',7),
+	 (3,'App\Models\User',8),
+	 (3,'App\Models\User',9),
+	 (3,'App\Models\User',10);
+INSERT INTO sispela.model_has_roles (role_id,model_type,model_id) VALUES
+	 (3,'App\Models\User',11),
+	 (3,'App\Models\User',12),
+	 (3,'App\Models\User',13),
+	 (3,'App\Models\User',14),
+	 (3,'App\Models\User',15),
+	 (3,'App\Models\User',16),
+	 (3,'App\Models\User',17),
+	 (3,'App\Models\User',18),
+	 (3,'App\Models\User',19),
+	 (3,'App\Models\User',20);
+INSERT INTO sispela.model_has_roles (role_id,model_type,model_id) VALUES
+	 (3,'App\Models\User',21),
+	 (3,'App\Models\User',22),
+	 (3,'App\Models\User',23),
+	 (3,'App\Models\User',24),
+	 (3,'App\Models\User',25),
+	 (3,'App\Models\User',26),
+	 (3,'App\Models\User',27),
+	 (3,'App\Models\User',28),
+	 (3,'App\Models\User',29),
+	 (3,'App\Models\User',30);
+INSERT INTO sispela.model_has_roles (role_id,model_type,model_id) VALUES
+	 (3,'App\Models\User',31),
+	 (3,'App\Models\User',32),
+	 (3,'App\Models\User',33),
+	 (3,'App\Models\User',34);
---
+++ database/sql/users_202501290236.sql
@@ -0,0 +1,38 @@
+INSERT INTO sispela.users (name,email,email_verified_at,password,profile_picture_path,remember_token,created_at,updated_at,deleted_at,program_studi_id,notification_email) VALUES
+	 ('Super Admin','superadmin@admin.com','2025-01-28 19:29:42','$2y$12$rRhEQcK2fRogd2Z3GM43AeGFKRQTJey51S4MRYfhwyOIvMPuhZn3y',NULL,'E01kDZftkW','2025-01-28 19:29:42','2025-01-28 19:29:42',NULL,NULL,0),
+	 ('Admin','admin@admin.com','2025-01-28 19:29:42','$2y$12$rRhEQcK2fRogd2Z3GM43AeGFKRQTJey51S4MRYfhwyOIvMPuhZn3y',NULL,'OoRo201dvk','2025-01-28 19:29:42','2025-01-28 19:29:42',NULL,NULL,0),
+	 ('Elang Permadani','elangpermadani123@gmail.com','2025-01-28 19:29:42','$2y$12$rRhEQcK2fRogd2Z3GM43AeGFKRQTJey51S4MRYfhwyOIvMPuhZn3y',NULL,'zcHOss1tIX','2025-01-28 19:29:42','2025-01-28 19:29:42',NULL,NULL,0),
+	 ('Andika Setiawan, S.Kom., M.Cs.','andika.setiawan@if.itera.ac.id',NULL,'$2y$12$4/LaK/wMm6tkD.LwN9LzdesBEEBsKgJRYtH91Y5PHl1GWtCbgu9c6',NULL,NULL,'2025-01-28 19:31:31','2025-01-28 19:31:31',NULL,24,0),
+	 ('Eko Dwi Nugroho, S.Kom., M.Cs.','eko.nugroho@if.itera.ac.id',NULL,'$2y$12$U3WK./3VIlHqgOlZ2YY4X.c8JwwKSMYopcNGHeJEaDO29wE8ZXqTe',NULL,NULL,'2025-01-28 19:31:31','2025-01-28 19:31:31',NULL,24,0),
+	 ('Muhammad Habib Algifari, S.Kom., M.TI.','muhammad.algifari@if.itera.ac.id',NULL,'$2y$12$Oq8KSiQ3C6lWy23lI3V..uOs.TQ8mMHfeJJ5ZGIl2zGyO2OFrED4y',NULL,NULL,'2025-01-28 19:31:32','2025-01-28 19:31:32',NULL,24,0),
+	 ('Winda Yulita, M.Cs.','winda.yulita@if.itera.ac.id',NULL,'$2y$12$LE9ez9WHI8t/yujewXujs.iOM4O7JVwHrXQqkNdX/jafH3QpwOJzK',NULL,NULL,'2025-01-28 19:31:32','2025-01-28 19:31:32',NULL,24,0),
+	 ('Radhinka Bagaskara, S.Si.Kom., M.Si., M.Sc.','radhinka.bagaskara@if.itera.ac.id',NULL,'$2y$12$WcmTBY2GJp5YoWfeaJlbA.K2AnW0CtB6iYgvW.t9zZrLE8s3yzpb.',NULL,NULL,'2025-01-28 19:31:32','2025-01-28 19:31:32',NULL,24,0),
+	 ('Meida Cahyo Untoro, S.Kom., M.Kom','cahyo.untoro@if.itera.ac.id',NULL,'$2y$12$A5Xm9//ZlCKmO6/b2QrKX.2GCychlpBYkuuY0Jb2.PkiM100oaweu',NULL,NULL,'2025-01-28 19:31:33','2025-01-28 19:31:33',NULL,24,0),
+	 ('Ilham Firman Ashari, S.Kom., M.T','firman.ashari@if.itera.ac.id',NULL,'$2y$12$.B57LbjtNCtvfsG41IhfnOGG0rGTrzD2r98peYkoJi5J2XnRF1JNO',NULL,NULL,'2025-01-28 19:31:33','2025-01-28 19:31:33',NULL,24,0);
+INSERT INTO sispela.users (name,email,email_verified_at,password,profile_picture_path,remember_token,created_at,updated_at,deleted_at,program_studi_id,notification_email) VALUES
+	 ('Leslie Anggraini, S.Kom., M.Cs.','leslie.anggraini@if.itera.ac.id',NULL,'$2y$12$WqnB6VvJ24flUd5kTMv.vuZ6cLVsftQw6K3anozqlJDTuc4Rk37pq',NULL,NULL,'2025-01-28 19:31:33','2025-01-28 19:31:33',NULL,24,0),
+	 ('Miranti Verdiana, M.Si.','miranti.verdiana@if.itera.ac.id',NULL,'$2y$12$.zGa63RKjrusfHEfxEln2uZHJa.24ucZskmULbc0sCumhLE1YUJGi',NULL,NULL,'2025-01-28 19:31:34','2025-01-28 19:31:34',NULL,24,0),
+	 ('Ir. Hira Laksmiwati Soemitro, M.Sc.','hira@informatika.org',NULL,'$2y$12$EIjNAIJicExc2G35xmvFK.X.GHak5/MQNYP.O329fRsfhuYIK/iw6',NULL,NULL,'2025-01-28 19:31:34','2025-01-28 19:31:34',NULL,24,0),
+	 ('Rajif Agung Yunmar, S.Kom., M.Cs.','rajif@if.itera.ac.id',NULL,'$2y$12$XEjeieUmeCLOOhkHZtxpX.g9R8hacB1W6jnCkR0wEh4HAnjnrcRha',NULL,NULL,'2025-01-28 19:31:34','2025-01-28 19:31:34',NULL,24,0),
+	 ('Raidah Hanifah, S.T., M.T.','raidah.hanifah@if.itera.ac.id',NULL,'$2y$12$I9ZoJGPhYO5/cb5OzWF/MulyhndVrIhqR5IKWziSuk77/6LwBA7Fm',NULL,NULL,'2025-01-28 19:31:35','2025-01-28 19:31:35',NULL,24,0),
+	 ('Arkham Zahri Rakhman S.Kom., M.Eng.','arkham@if.itera.ac.id',NULL,'$2y$12$QH0DHk0XcW69yltQTVVDjeEODRsS0y63l/3ezO3gh2tyAFmVbWX/K',NULL,NULL,'2025-01-28 19:31:35','2025-01-28 19:31:35',NULL,24,0),
+	 ('Rahman Indra Kesuma, S.Kom., M.Cs.','rahman.indra@if.itera.ac.id',NULL,'$2y$12$ycoMoKdCx5h2slxhL3v3rete7Rl0wnIkWPKqsQfQvl/Rkql/I4c/O',NULL,NULL,'2025-01-28 19:31:35','2025-01-28 19:31:35',NULL,24,0),
+	 ('Hafiz Budi Firmansyah S.Kom., M.Sc.','hafiz.budi@if.itera.ac.id',NULL,'$2y$12$JkZ/0vilTZB6nqeP30Rj9O3FqHl38uuMTLwgIkI1dqF4H3yWZhCwC',NULL,NULL,'2025-01-28 19:31:36','2025-01-28 19:31:36',NULL,24,0),
+	 ('I Wayan Wiprayoga Wisesa S.Kom., M.Kom','wayan.wisesa@if.itera.ac.id',NULL,'$2y$12$aaXYfy2XV46cR2x6WCfIKu7MizMmDMYVOZCbsq3KMF.grxTc/zWrO',NULL,NULL,'2025-01-28 19:31:36','2025-01-28 19:31:36',NULL,24,0),
+	 ('Imam Ekowicaksono, S.Si., M.Si.','imam.wicaksono@if.itera.ac.id',NULL,'$2y$12$kkjI9dtuuMaXwXa95Eg83OFa.djRowx7BNSQorX.g/jOoDq9CYg5S',NULL,NULL,'2025-01-28 19:31:36','2025-01-28 19:31:36',NULL,24,0);
+INSERT INTO sispela.users (name,email,email_verified_at,password,profile_picture_path,remember_token,created_at,updated_at,deleted_at,program_studi_id,notification_email) VALUES
+	 ('Hartanto Tantriawan, S.Kom., M.Kom.','hartanto.tantriawan@if.itera.ac.id',NULL,'$2y$12$awwIh2Wh7QMQvj/bbJeNO.NWrlQWO6VjCEFDszVHul2LyoLqKCnb2',NULL,NULL,'2025-01-28 19:31:37','2025-01-28 19:31:37',NULL,24,0),
+	 ('Angga Wijaya S.Si., M.Si.','angga.wijaya@if.itera.ac.id',NULL,'$2y$12$AGNei9Km3WBG8fTQsK/2gepA1vuhBANMgFqs1I/zLuC5xRKDCorFa',NULL,NULL,'2025-01-28 19:31:37','2025-01-28 19:31:37',NULL,24,0),
+	 ('Amirul Iqbal S.Kom., M.Eng.','amirul.iqbal@if.itera.ac.id',NULL,'$2y$12$eFshMmPRiSFtWUBnDUq7vecWnJu.ccrAXB8rbAbRqjIRofMhbf4aS',NULL,NULL,'2025-01-28 19:31:37','2025-01-28 19:31:37',NULL,24,0),
+	 ('Mohamad Idris S.Si., M.Sc.','mohamad.idris@if.itera.ac.id',NULL,'$2y$12$a6fJ024VCCrv44l5D84zyekV4UlIZNiqHrp3HnB9B/hhxKB97KS8m',NULL,NULL,'2025-01-28 19:31:38','2025-01-28 19:31:38',NULL,24,0),
+	 ('Arief Ichwani S.Kom., M.Cs.','arief.ichwani@if.itera.ac.id',NULL,'$2y$12$KwOYP/xBN14AVOElNxe..Ovn5VywKDGoCbNemPkw9jCNrU48C7rcG',NULL,NULL,'2025-01-28 19:31:38','2025-01-28 19:31:38',NULL,24,0),
+	 ('Martin C.T. Manullang S.T., M.T.','martin.manullang@if.itera.ac.id',NULL,'$2y$12$lGLjGMwZW6efzuBi/sddk.S5clz4mGu0z1zAXnAT.goP4NoPffTba',NULL,NULL,'2025-01-28 19:31:38','2025-01-28 19:31:38',NULL,24,0),
+	 ('Ir. Mugi Praseptiawan S.T., M.Kom','mugi.praseptiawan@if.itera.ac.id',NULL,'$2y$12$hd/s3jycfWRcz/InVUA25uqqR91YeJL46KEj4QNuwCFCXolBBxyru',NULL,NULL,'2025-01-28 19:31:39','2025-01-28 19:31:39',NULL,24,0),
+	 ('Andre Febrianto S.Kom., M.Eng','andre.febrianto@if.itera.ac.id',NULL,'$2y$12$5jpVu1e1gDIzTiX4xOW85.xeZOc7lTpN.Iildy7SeXd3ZRXzMtkm6',NULL,NULL,'2025-01-28 19:31:39','2025-01-28 19:31:39',NULL,24,0),
+	 ('Aidil Afriansyah, S.Kom., M.Kom.','aidil.afriansyah@if.itera.ac.id',NULL,'$2y$12$Bmr1ycj9NMGr9qjVI4pNNuY6YyFIonEUqb3mo0ZSGeZqpBwXMDI2C',NULL,NULL,'2025-01-28 19:31:39','2025-01-28 19:31:39',NULL,24,0),
+	 ('Prof. Sarwono Sutikno, Dr,Eng., CISA, CISSP, CISM, CSX-F, IIAP, CC','sarwono.sutikno@if.itera.ac.id',NULL,'$2y$12$FfFFOBtnQqE7XAgOdw2bV...mJif39mcGifyCnesEHCwsnBKdHRKa',NULL,NULL,'2025-01-28 19:31:40','2025-01-28 19:31:40',NULL,24,0);
+INSERT INTO sispela.users (name,email,email_verified_at,password,profile_picture_path,remember_token,created_at,updated_at,deleted_at,program_studi_id,notification_email) VALUES
+	 ('Siswanto, S.Pd., M.Si.','siswanto@if.itera.ac.id',NULL,'$2y$12$h5f/fkLCpN90cC7huxJK6u4FfngQf6Qy43JGl27lmZHQhMnp0u1FW',NULL,NULL,'2025-01-28 19:31:40','2025-01-28 19:31:40',NULL,24,0),
+	 ('Ade Setiawan, S.Si.','ade.setiawan@staff.itera.ac.id',NULL,'$2y$12$BUxkKYKAKo85hTHvl3tGJeboXcJdqAZv.c8NCZH3qtHpD.tgQ4qLy',NULL,NULL,'2025-01-28 19:31:40','2025-01-28 19:31:40',NULL,24,0),
+	 ('M. Fitrah Ramadhan, S.Kom.','fitrah.ramadhan@staff.itera.ac.id',NULL,'$2y$12$BMwUojcDhvk/fRvndl6WduF8GiV1ay63vo51B6jVzY/nQFgy0ZLdu',NULL,NULL,'2025-01-28 19:31:41','2025-01-28 19:31:41',NULL,24,0),
+	 ('Dirga Rama Sudira, S.T.','dirga.sudira@staff.itera.ac.id',NULL,'$2y$12$tPyno1RK8SVsQCfUI5W4nuhHcJqXeMt7bicYwwpF2PHIPTLw7zmEu',NULL,NULL,'2025-01-28 19:31:41','2025-01-28 19:31:41',NULL,24,0);
